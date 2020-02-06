2150436a305722dfdac05f09d1ffef2fe04d8882
Mark Bihag <markbihag330@gmail.com>
2020-02-06 06:17:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,21 @@
-
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+ 
+  def self.all
+    @@all
+  end
+ 
+  def save
+    self.class.all << self
+  end
+  
+  def self.create(song_title)
+    save
+  end
+  
+  def self.new_by_name(name)
+    @name=(name)
+  end
+ 
+end
